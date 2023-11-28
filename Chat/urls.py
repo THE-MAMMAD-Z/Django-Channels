fd59510214bcb265ed5137835e2e892711a837f4
mohammad <mohammadz270@yahoo.com>
2023-11-28 20:40:21 +0330

    firstcommit

---
+++ Chat/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from .views import index , room
+
+urlpatterns = [
+    path("",index ),
+    path("<str:room_name>/", room, name="room"),
+]
